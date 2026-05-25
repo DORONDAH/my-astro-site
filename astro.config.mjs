3a38af267b9e2d71fe19ad741926705c22cf82d7
DORONDAH <DORONDAH@users.noreply.github.com>
2026-05-26 00:18:58 +0300

    Update site and base URL for Github Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://<USERNAME>.github.io',
-  base: '/<REPO-NAME>',
+  site: 'https://DORONDAH.github.io',
+  base: '/my-astro-site',
 });
